322605cf4592d34b470a26d67bded9087a0712c0
Hidetora <idetoratojo1@gmail.com>
2024-03-05 14:59:15 +0100

    fixing lint errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -11,7 +11,7 @@ const InvoiceDTO = z.object({
   }),
   amount: z.coerce
     .number()
-    .gt(0, { message: 'Please enter an amount greater than $0.' }),,
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
   status: z.enum(['pending', 'paid'], {
     invalid_type_error: 'Please select an invoice status.',
   }),
